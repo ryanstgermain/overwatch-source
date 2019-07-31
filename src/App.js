e5ef85d2085f4f338fdbd66831960285c29a218f
ryanstgermain <ryanjoel26@gmail.com>
2019-07-31 13:39:24 -0600

    Got the basic layout for the header component.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './style/App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
 import Footer from './components/Footer';
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <h4>Header</h4>
+    <Router>
+      <div className='header'>
+        <div>
+          <h1>
+            <Link to='/'>Overwatch Source</Link>
+          </h1>  
+        </div>
+        <div>
+          <h3>
+            <Link to='/about-overwatch'>About Overwatch</Link>
+          </h3>
+          <h3>
+            <Link to='/about-us'>About Us</Link>
+          </h3>
+          <h3>
+            <Link to='/contact-us'>Contact Us</Link>
+          </h3>
+        </div>
+        {/* <Route path='/about-overwatch' component={AboutOverwatch} /> */}
+        {/* <Route path='/about-us/' component={AboutPage} /> */}
+      </div>  
+    </Router>
+    
   );
 }
 
