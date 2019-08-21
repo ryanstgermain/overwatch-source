07ba9e881ca5b328457c9e5df5491bfabe76b595
ryanstgermain <ryanjoel26@gmail.com>
2019-08-21 11:28:34 -0600

    Got the basic layout for the hero list page.

--- src/components/heros/list/Support.jsx
+++ src/components/heros/list/Support.jsx
@@ -1,11 +1,96 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
-// import { Divider } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import { Image, Divider } from 'semantic-ui-react';
 
 const Support = () => {
   return (
     <div>
+      <Divider hidden />
       <h2 className='hero-list-heading'>Support</h2>
+      <Divider />
+      <div className='align-list-center'>
+        <div className='hero-list'>
+          <div className='hero'>
+            <Link to='/hero'>
+              <Image src='https://upload.wikimedia.org/wikipedia/en/4/4c/Hanzo_Overwatch.png' size='small' />    
+            </Link>
+            <span>Hanzo</span>    
+          </div>
+          <div className='hero'>
+            <Link to='/hero'>
+              <Image src='https://upload.wikimedia.org/wikipedia/en/4/4c/Hanzo_Overwatch.png' size='small' />    
+            </Link>
+            <span>Hanzo</span>    
+          </div>
+          <div className='hero'>
+            <Link to='/hero'>
+              <Image src='https://upload.wikimedia.org/wikipedia/en/4/4c/Hanzo_Overwatch.png' size='small' />    
+            </Link>
+            <span>Hanzo</span>    
+          </div>
+          <div className='hero'>
+            <Link to='/hero'>
+              <Image src='https://upload.wikimedia.org/wikipedia/en/4/4c/Hanzo_Overwatch.png' size='small' />    
+            </Link>
+            <span>Hanzo</span>    
+          </div>
+          <div className='hero'>
+            <Link to='/hero'>
+              <Image src='https://upload.wikimedia.org/wikipedia/en/4/4c/Hanzo_Overwatch.png' size='small' />    
+            </Link>
+            <span>Hanzo</span>    
+          </div>
+          <div className='hero'>
+            <Link to='/hero'>
+              <Image src='https://upload.wikimedia.org/wikipedia/en/4/4c/Hanzo_Overwatch.png' size='small' />    
+            </Link>
+            <span>Hanzo</span>    
+          </div>
+          <div className='hero'>
+            <Link to='/hero'>
+              <Image src='https://upload.wikimedia.org/wikipedia/en/4/4c/Hanzo_Overwatch.png' size='small' />    
+            </Link>
+            <span>Hanzo</span>    
+          </div>
+          <div className='hero'>
+            <Link to='/hero'>
+              <Image src='https://upload.wikimedia.org/wikipedia/en/4/4c/Hanzo_Overwatch.png' size='small' />    
+            </Link>
+            <span>Hanzo</span>    
+          </div>
+          <div className='hero'>
+            <Link to='/hero'>
+              <Image src='https://upload.wikimedia.org/wikipedia/en/4/4c/Hanzo_Overwatch.png' size='small' />    
+            </Link>
+            <span>Hanzo</span>    
+          </div>
+          <div className='hero'>
+            <Link to='/hero'>
+              <Image src='https://upload.wikimedia.org/wikipedia/en/4/4c/Hanzo_Overwatch.png' size='small' />    
+            </Link>
+            <span>Hanzo</span>    
+          </div>
+          <div className='hero'>
+            <Link to='/hero'>
+              <Image src='https://upload.wikimedia.org/wikipedia/en/4/4c/Hanzo_Overwatch.png' size='small' />    
+            </Link>
+            <span>Hanzo</span>    
+          </div>
+          <div className='hero'>
+            <Link to='/hero'>
+              <Image src='https://upload.wikimedia.org/wikipedia/en/4/4c/Hanzo_Overwatch.png' size='small' />    
+            </Link>
+            <span>Hanzo</span>    
+          </div>
+          <div className='hero'>
+            <Link to='/hero'>
+              <Image src='https://upload.wikimedia.org/wikipedia/en/4/4c/Hanzo_Overwatch.png' size='small' />    
+            </Link>
+            <span>Hanzo</span>    
+          </div>
+        </div>
+      </div>
+      <Divider section hidden />    
     </div> 
   );
 }
