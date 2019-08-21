90dea890e20758c52209c3f114470142de9aa411
ryanstgermain <ryanjoel26@gmail.com>
2019-08-21 14:02:11 -0600

    Changed some things with styling in the overview section on heros list.

--- src/components/heros/list/Tank.jsx
+++ src/components/heros/list/Tank.jsx
@@ -4,7 +4,7 @@ import { Image, Divider } from 'semantic-ui-react';
 
 const Tank = () => {
   return (
-    <div className='test'>
+    <div className='heros-margin-top'>
       <Divider />
       <h2 className='hero-list-heading'>Tank</h2>
       <div className='align-list-center'>
