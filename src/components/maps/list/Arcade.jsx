954140af2dbf3337cd1a97cc5562f36ddb076acc
ryanstgermain <ryanjoel26@gmail.com>
2019-08-26 15:04:38 -0600

    Cleaned up code a bit.

--- src/components/maps/list/Arcade.jsx
+++ src/components/maps/list/Arcade.jsx
@@ -12,84 +12,84 @@ const Arcade = () => {
         </p>
         <Divider section className='divider-mobile-hidden' />    
       </div>
-        <div className='map-row'>
-          <div className='map-arcade-width'>
-            <h3 className='map-type'>Elimination</h3>
-            <p className='map-type-overview'>
-              Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-            </p>
-            <Divider />
-            <div className='maps-list'>
-              <div className='map'>
-                <Link to='/map'>
-                  <Image src='https://gamepedia.cursecdn.com/overwatch_gamepedia/thumb/7/7c/Horizon_Lunar_Colony2.jpg/1600px-Horizon_Lunar_Colony2.jpg?version=93768bc0b649b500598b55280a3792b6' size='small' />
-                </Link>
-                <span className='map-font'>Horizon Lunar Colony</span>
-                <Divider fitted />
-              </div>
-              <div className='map'>
-                <Link to='/map'>
-                  <Image src='https://gamepedia.cursecdn.com/overwatch_gamepedia/thumb/7/7c/Horizon_Lunar_Colony2.jpg/1600px-Horizon_Lunar_Colony2.jpg?version=93768bc0b649b500598b55280a3792b6' size='small' />
-                </Link>
-                <span className='map-font'>Horizon Lunar Colony</span>
-                <Divider fitted />
-              </div>
-              <div className='map'>
-                <Link to='/map'>
-                  <Image src='https://gamepedia.cursecdn.com/overwatch_gamepedia/thumb/7/7c/Horizon_Lunar_Colony2.jpg/1600px-Horizon_Lunar_Colony2.jpg?version=93768bc0b649b500598b55280a3792b6' size='small' />
-                </Link>
-                <span className='map-font'>Horizon Lunar Colony</span>
-                <Divider fitted />
-              </div>
-              <div className='map'>
-                <Link to='/map'>
-                  <Image src='https://gamepedia.cursecdn.com/overwatch_gamepedia/thumb/7/7c/Horizon_Lunar_Colony2.jpg/1600px-Horizon_Lunar_Colony2.jpg?version=93768bc0b649b500598b55280a3792b6' size='small' />
-                </Link>
-                <span className='map-font'>Horizon Lunar Colony</span>
-                <Divider fitted />
-              </div>
+      <div className='map-row'>
+        <div className='map-arcade-width'>
+          <h3 className='map-type'>Elimination</h3>
+          <p className='map-type-overview'>
+            Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+          </p>
+          <Divider />
+          <div className='maps-list'>
+            <div className='map'>
+              <Link to='/map'>
+                <Image src='https://gamepedia.cursecdn.com/overwatch_gamepedia/thumb/7/7c/Horizon_Lunar_Colony2.jpg/1600px-Horizon_Lunar_Colony2.jpg?version=93768bc0b649b500598b55280a3792b6' size='small' />
+              </Link>
+              <span className='map-font'>Horizon Lunar Colony</span>
+              <Divider fitted />
+            </div>
+            <div className='map'>
+              <Link to='/map'>
+                <Image src='https://gamepedia.cursecdn.com/overwatch_gamepedia/thumb/7/7c/Horizon_Lunar_Colony2.jpg/1600px-Horizon_Lunar_Colony2.jpg?version=93768bc0b649b500598b55280a3792b6' size='small' />
+              </Link>
+              <span className='map-font'>Horizon Lunar Colony</span>
+              <Divider fitted />
+            </div>
+            <div className='map'>
+              <Link to='/map'>
+                <Image src='https://gamepedia.cursecdn.com/overwatch_gamepedia/thumb/7/7c/Horizon_Lunar_Colony2.jpg/1600px-Horizon_Lunar_Colony2.jpg?version=93768bc0b649b500598b55280a3792b6' size='small' />
+              </Link>
+              <span className='map-font'>Horizon Lunar Colony</span>
+              <Divider fitted />
+            </div>
+            <div className='map'>
+              <Link to='/map'>
+                <Image src='https://gamepedia.cursecdn.com/overwatch_gamepedia/thumb/7/7c/Horizon_Lunar_Colony2.jpg/1600px-Horizon_Lunar_Colony2.jpg?version=93768bc0b649b500598b55280a3792b6' size='small' />
+              </Link>
+              <span className='map-font'>Horizon Lunar Colony</span>
+              <Divider fitted />
             </div>
           </div>
-          <div className='map-arcade-width'>
-            <h3 className='map-type'>Deathmatch</h3>
-            <p className='map-type-overview'>
-              Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-            </p>
-            <Divider />
-            <div className='maps-list'>
-              <div className='map'>
-                <Link to='/map'>
-                  <Image src='https://gamepedia.cursecdn.com/overwatch_gamepedia/thumb/7/7c/Horizon_Lunar_Colony2.jpg/1600px-Horizon_Lunar_Colony2.jpg?version=93768bc0b649b500598b55280a3792b6' size='small' />
-                </Link>
-                <span className='map-font'>Horizon Lunar Colony</span>
-                <Divider fitted />
-              </div>
-              <div className='map'>
-                <Link to='/map'>
-                  <Image src='https://gamepedia.cursecdn.com/overwatch_gamepedia/thumb/7/7c/Horizon_Lunar_Colony2.jpg/1600px-Horizon_Lunar_Colony2.jpg?version=93768bc0b649b500598b55280a3792b6' size='small' />
-                </Link>
-                <span className='map-font'>Horizon Lunar Colony</span>
-                <Divider fitted />
-              </div>
-            </div>
-          </div>
-          <div className='map-arcade-width'>
-            <h3 className='map-type'>Capture the Flag</h3>
-            <p className='map-type-overview'>
-              Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-            </p>
-            <Divider />
-            <div className='maps-list'>
-              <div className='map'>
-                <Link to='/map'>
-                  <Image src='https://gamepedia.cursecdn.com/overwatch_gamepedia/thumb/7/7c/Horizon_Lunar_Colony2.jpg/1600px-Horizon_Lunar_Colony2.jpg?version=93768bc0b649b500598b55280a3792b6' size='small' />
-                </Link>
-                <span className='map-font'>Horizon Lunar Colony</span>
-                <Divider fitted />
-              </div>  
-            </div>
-          </div>    
         </div>
+        <div className='map-arcade-width'>
+          <h3 className='map-type'>Deathmatch</h3>
+          <p className='map-type-overview'>
+            Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+          </p>
+          <Divider />
+          <div className='maps-list'>
+            <div className='map'>
+              <Link to='/map'>
+                <Image src='https://gamepedia.cursecdn.com/overwatch_gamepedia/thumb/7/7c/Horizon_Lunar_Colony2.jpg/1600px-Horizon_Lunar_Colony2.jpg?version=93768bc0b649b500598b55280a3792b6' size='small' />
+              </Link>
+              <span className='map-font'>Horizon Lunar Colony</span>
+              <Divider fitted />
+            </div>
+            <div className='map'>
+              <Link to='/map'>
+                <Image src='https://gamepedia.cursecdn.com/overwatch_gamepedia/thumb/7/7c/Horizon_Lunar_Colony2.jpg/1600px-Horizon_Lunar_Colony2.jpg?version=93768bc0b649b500598b55280a3792b6' size='small' />
+              </Link>
+              <span className='map-font'>Horizon Lunar Colony</span>
+              <Divider fitted />
+            </div>
+          </div>
+        </div>
+        <div className='map-arcade-width'>
+          <h3 className='map-type'>Capture the Flag</h3>
+          <p className='map-type-overview'>
+            Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+          </p>
+          <Divider />
+          <div className='maps-list'>
+            <div className='map'>
+              <Link to='/map'>
+                <Image src='https://gamepedia.cursecdn.com/overwatch_gamepedia/thumb/7/7c/Horizon_Lunar_Colony2.jpg/1600px-Horizon_Lunar_Colony2.jpg?version=93768bc0b649b500598b55280a3792b6' size='small' />
+              </Link>
+              <span className='map-font'>Horizon Lunar Colony</span>
+              <Divider fitted />
+            </div>  
+          </div>
+        </div>    
+      </div>
       <Divider className='divider-desktop-hidden' />
     </div>
   );
