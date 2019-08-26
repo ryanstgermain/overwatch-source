f1012260693ba2ee0059e7f8a8d7fd4d9b334dc8
ryanstgermain <ryanjoel26@gmail.com>
2019-08-26 16:35:45 -0600

    Added a section for other arcade maps.

--- src/components/maps/list/OtherArcade.jsx
+++ src/components/maps/list/OtherArcade.jsx
@@ -4,8 +4,145 @@ import { Image, Divider } from 'semantic-ui-react';
 
 const OtherArcade = () => {
   return (
-    <div>
-      {/* <h2>Other arcade Maps</h2> */}
+    <div className='test'>
+      <div className='map-margin-top'>
+        <h2 className='map-category-title'>Other Arcade Maps</h2>
+        <p className='map-category'>
+          Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+        </p>
+        <Divider section className='divider-mobile-hidden' />    
+      </div>
+      <div className='map-row-arcade-other'>
+        <div className='map-arcade-width-other'>
+          <h3 className='map-type-arcade-other'>Ilios</h3>
+          <Divider />
+          <div className='maps-list-arcade-other'>
+            <div className='map'>
+              <Link to='/map'>
+                <Image src='https://gamepedia.cursecdn.com/overwatch_gamepedia/thumb/7/7c/Horizon_Lunar_Colony2.jpg/1600px-Horizon_Lunar_Colony2.jpg?version=93768bc0b649b500598b55280a3792b6' size='small' />
+              </Link>
+              <span className='map-font'>Horizon Lunar Colony</span>
+              <Divider fitted />
+            </div>  
+            <div className='map'>
+              <Link to='/map'>
+                <Image src='https://gamepedia.cursecdn.com/overwatch_gamepedia/thumb/7/7c/Horizon_Lunar_Colony2.jpg/1600px-Horizon_Lunar_Colony2.jpg?version=93768bc0b649b500598b55280a3792b6' size='small' />
+              </Link>
+              <span className='map-font'>Horizon Lunar Colony</span>
+              <Divider fitted />
+            </div>   
+            <div className='map'>
+              <Link to='/map'>
+                <Image src='https://gamepedia.cursecdn.com/overwatch_gamepedia/thumb/7/7c/Horizon_Lunar_Colony2.jpg/1600px-Horizon_Lunar_Colony2.jpg?version=93768bc0b649b500598b55280a3792b6' size='small' />
+              </Link>
+              <span className='map-font'>Horizon Lunar Colony</span>
+              <Divider fitted />
+            </div>
+          </div>
+        </div> 
+        <div className='map-arcade-width-other'>
+          <h3 className='map-type-arcade-other'>Lijiang Tower</h3>
+          <Divider />
+          <div className='maps-list-arcade-other'>
+            <div className='map'>
+              <Link to='/map'>
+                <Image src='https://gamepedia.cursecdn.com/overwatch_gamepedia/thumb/7/7c/Horizon_Lunar_Colony2.jpg/1600px-Horizon_Lunar_Colony2.jpg?version=93768bc0b649b500598b55280a3792b6' size='small' />
+              </Link>
+              <span className='map-font'>Horizon Lunar Colony</span>
+              <Divider fitted />
+            </div> 
+            <div className='map'>
+              <Link to='/map'>
+                <Image src='https://gamepedia.cursecdn.com/overwatch_gamepedia/thumb/7/7c/Horizon_Lunar_Colony2.jpg/1600px-Horizon_Lunar_Colony2.jpg?version=93768bc0b649b500598b55280a3792b6' size='small' />
+              </Link>
+              <span className='map-font'>Horizon Lunar Colony</span>
+              <Divider fitted />
+            </div>     
+            <div className='map'>
+              <Link to='/map'>
+                <Image src='https://gamepedia.cursecdn.com/overwatch_gamepedia/thumb/7/7c/Horizon_Lunar_Colony2.jpg/1600px-Horizon_Lunar_Colony2.jpg?version=93768bc0b649b500598b55280a3792b6' size='small' />
+              </Link>
+              <span className='map-font'>Horizon Lunar Colony</span>
+              <Divider fitted />
+            </div>
+          </div>
+        </div>
+        <div className='map-arcade-width-other'>
+          <h3 className='map-type-arcade-other'>Busan</h3>
+          <Divider />
+          <div className='maps-list-arcade-other'>
+            <div className='map'>
+              <Link to='/map'>
+                <Image src='https://gamepedia.cursecdn.com/overwatch_gamepedia/thumb/7/7c/Horizon_Lunar_Colony2.jpg/1600px-Horizon_Lunar_Colony2.jpg?version=93768bc0b649b500598b55280a3792b6' size='small' />
+              </Link>
+              <span className='map-font'>Horizon Lunar Colony</span>
+              <Divider fitted />
+            </div>
+            <div className='map'>
+              <Link to='/map'>
+                <Image src='https://gamepedia.cursecdn.com/overwatch_gamepedia/thumb/7/7c/Horizon_Lunar_Colony2.jpg/1600px-Horizon_Lunar_Colony2.jpg?version=93768bc0b649b500598b55280a3792b6' size='small' />
+              </Link>
+              <span className='map-font'>Horizon Lunar Colony</span>
+              <Divider fitted />
+            </div>
+          </div>
+        </div> 
+        <div className='map-arcade-width-other'>
+          <h3 className='map-type-arcade-other'>Nepal</h3>
+          <Divider />
+          <div className='maps-list-arcade-other'>
+            <div className='map'>
+              <Link to='/map'>
+                <Image src='https://gamepedia.cursecdn.com/overwatch_gamepedia/thumb/7/7c/Horizon_Lunar_Colony2.jpg/1600px-Horizon_Lunar_Colony2.jpg?version=93768bc0b649b500598b55280a3792b6' size='small' />
+              </Link>
+              <span className='map-font'>Horizon Lunar Colony</span>
+              <Divider fitted />
+            </div>   
+            <div className='map'>
+              <Link to='/map'>
+                <Image src='https://gamepedia.cursecdn.com/overwatch_gamepedia/thumb/7/7c/Horizon_Lunar_Colony2.jpg/1600px-Horizon_Lunar_Colony2.jpg?version=93768bc0b649b500598b55280a3792b6' size='small' />
+              </Link>
+              <span className='map-font'>Horizon Lunar Colony</span>
+              <Divider fitted />
+            </div>   
+            <div className='map'>
+              <Link to='/map'>
+                <Image src='https://gamepedia.cursecdn.com/overwatch_gamepedia/thumb/7/7c/Horizon_Lunar_Colony2.jpg/1600px-Horizon_Lunar_Colony2.jpg?version=93768bc0b649b500598b55280a3792b6' size='small' />
+              </Link>
+              <span className='map-font'>Horizon Lunar Colony</span>
+              <Divider fitted />
+            </div>
+          </div>
+        </div> 
+        <div className='map-arcade-width-other'>
+          <h3 className='map-type-arcade-other'>Oasis</h3>
+          <Divider />
+          <div className='maps-list-arcade-other'>
+            <div className='map'>
+              <Link to='/map'>
+                <Image src='https://gamepedia.cursecdn.com/overwatch_gamepedia/thumb/7/7c/Horizon_Lunar_Colony2.jpg/1600px-Horizon_Lunar_Colony2.jpg?version=93768bc0b649b500598b55280a3792b6' size='small' />
+              </Link>
+              <span className='map-font'>Horizon Lunar Colony</span>
+              <Divider fitted />
+            </div>   
+            <div className='map'>
+              <Link to='/map'>
+                <Image src='https://gamepedia.cursecdn.com/overwatch_gamepedia/thumb/7/7c/Horizon_Lunar_Colony2.jpg/1600px-Horizon_Lunar_Colony2.jpg?version=93768bc0b649b500598b55280a3792b6' size='small' />
+              </Link>
+              <span className='map-font'>Horizon Lunar Colony</span>
+              <Divider fitted />
+            </div>   
+            <div className='map'>
+              <Link to='/map'>
+                <Image src='https://gamepedia.cursecdn.com/overwatch_gamepedia/thumb/7/7c/Horizon_Lunar_Colony2.jpg/1600px-Horizon_Lunar_Colony2.jpg?version=93768bc0b649b500598b55280a3792b6' size='small' />
+              </Link>
+              <span className='map-font'>Horizon Lunar Colony</span>
+              <Divider fitted />
+            </div> 
+          </div>
+        </div> 
+      </div>
+      <Divider className='divider-desktop-hidden' />
     </div>
   );
 }
