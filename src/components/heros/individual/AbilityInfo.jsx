68cc7596e72b125adfcd3c92bb4e29223b77497c
ryanstgermain <ryanjoel26@gmail.com>
2019-09-05 13:31:27 -0600

    Added sections for weapon info and ability info.

--- src/components/heros/individual/AbilityInfo.jsx
+++ src/components/heros/individual/AbilityInfo.jsx
@@ -4,11 +4,52 @@ import { Divider } from 'semantic-ui-react';
 const AbilityInfo = () => {
   return (
     <div>
-      <h2 className='maps-used'>ability info</h2>
       <Divider section hidden />
-      <div className='section-competitive'>
+      <div className='section-competitive weapons-section'>
         <div>
-            
+          <h2 className='maps-used'>Ability</h2>
+          <Divider />
+          <h3 className='hero-stat-text'>ability title</h3>
+        </div>
+        <div>
+          <h2 className='maps-used'>Ability Stats</h2>
+          <Divider />
+          <div className='hero-stats'>
+            <h3 className='hero-stat-text'>Type:</h3>
+            <span className='hero-stat-text-result'>ability type</span>    
+          </div>
+          <div className='hero-stats'>
+            <h3 className='hero-stat-text'>Health:</h3>
+            <span className='hero-stat-text-result'>200</span>    
+          </div>
+          <div className='hero-stats'>
+            <h3 className='hero-stat-text'>Damage:</h3>
+            <span className='hero-stat-text-result'>20</span>    
+          </div>
+          <div className='hero-stats'>
+            <h3 className='hero-stat-text'>Ammo:</h3>
+            <span className='hero-stat-text-result'></span>    
+          </div>
+          <div className='hero-stats'>
+            <h3 className='hero-stat-text'>Duration:</h3>
+            <span className='hero-stat-text-result'></span>    
+          </div>
+          <div className='hero-stats'>
+            <h3 className='hero-stat-text'>Cooldown:</h3>
+            <span className='hero-stat-text-result'></span>    
+          </div>
+          <div className='hero-stats'>
+            <h3 className='hero-stat-text'>Headshot:</h3>
+            <span className='hero-stat-text-result'>True</span>    
+          </div>
+        </div>
+        <div className='weapon-details'>
+          <h2 className='maps-used'>Weapon Details</h2>
+          <Divider />
+          <p className='hero-bio-text'>
+            Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum
+          </p>
+          <Divider />
         </div>
       </div>
     </div>
--- src/components/heros/individual/WeaponInfo.jsx
+++ src/components/heros/individual/WeaponInfo.jsx
@@ -4,11 +4,52 @@ import { Divider } from 'semantic-ui-react';
 const WeaponInfo = () => {
   return (
     <div>
-      <h2 className='maps-used'>weapon info</h2>
       <Divider section hidden />
-      <div className='section-competitive'>
+      <div className='section-competitive weapons-section'>
         <div>
-
+          <h2 className='maps-used'>Weapon</h2>
+          <Divider />
+          <h3 className='hero-stat-text'>weapon title</h3>
+        </div>
+        <div>
+          <h2 className='maps-used'>Weapon Stats</h2>
+          <Divider />
+          <div className='hero-stats'>
+            <h3 className='hero-stat-text'>Type:</h3>
+            <span className='hero-stat-text-result'>weapon type</span>    
+          </div>
+          <div className='hero-stats'>
+            <h3 className='hero-stat-text'>Damage:</h3>
+            <span className='hero-stat-text-result'>20</span>    
+          </div>
+          <div className='hero-stats'>
+            <h3 className='hero-stat-text'>Projectile Speed:</h3>
+            <span className='hero-stat-text-result'>40 meters per second</span>    
+          </div>
+          <div className='hero-stats'>
+            <h3 className='hero-stat-text'>Rate of Fire:</h3>
+            <span className='hero-stat-text-result'></span>    
+          </div>
+          <div className='hero-stats'>
+            <h3 className='hero-stat-text'>Ammo:</h3>
+            <span className='hero-stat-text-result'></span>    
+          </div>
+          <div className='hero-stats'>
+            <h3 className='hero-stat-text'>Reload Time:</h3>
+            <span className='hero-stat-text-result'></span>    
+          </div>
+          <div className='hero-stats'>
+            <h3 className='hero-stat-text'>Headshot:</h3>
+            <span className='hero-stat-text-result'></span>    
+          </div>
+        </div>
+        <div className='weapon-details'>
+          <h2 className='maps-used'>Weapon Details</h2>
+          <Divider />
+          <p className='hero-bio-text'>
+            Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum
+          <Divider />
+          </p>
         </div>
       </div>
     </div>
